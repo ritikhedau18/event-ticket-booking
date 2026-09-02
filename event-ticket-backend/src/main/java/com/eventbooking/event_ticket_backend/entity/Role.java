eb7066f3e50a8c63c8358a40a7bd3830885ba45f
ritikhedau18 <ritikhedau18@gmail.com>
2026-09-02 20:13:37 +0530

    feat: add user entity, role enum, and repository

---
+++ event-ticket-backend/src/main/java/com/eventbooking/event_ticket_backend/entity/Role.java
@@ -0,0 +1,7 @@
+package com.eventbooking.event_ticket_backend.entity;
+
+public enum  Role {
+
+    ADMIN, ORGANIZER, CUSTOMER
+
+}
---
+++ event-ticket-backend/src/main/java/com/eventbooking/event_ticket_backend/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.eventbooking.event_ticket_backend.repository;
+
+import com.eventbooking.event_ticket_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+}
