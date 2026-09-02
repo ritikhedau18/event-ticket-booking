20e6b18821399ea45f714bc3a9072182285425e7
ritikhedau18 <ritikhedau18@gmail.com>
2026-09-02 20:09:49 +0530

    chore: initialize Spring Boot backend

---
+++ event-ticket-backend/src/main/java/com/eventbooking/event_ticket_backend/EventTicketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventbooking.event_ticket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketBackendApplication.class, args);
+	}
+
+}
---
+++ event-ticket-backend/src/test/java/com/eventbooking/event_ticket_backend/EventTicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventbooking.event_ticket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
